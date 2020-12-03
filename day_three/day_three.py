91279c760fab6cc301afe8fcfc7e04679c0e23f7
Samuele Valente <samuelevalente@MacBook-Pro.local>
2020-12-03 18:21:15 +0100

    add three day solution

---
+++ day_three/day_three.py
@@ -0,0 +1,27 @@
+def read_input(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f]
+
+if __name__ == "__main__":
+    mountain = read_input('in.txt')
+    len_row = len(mountain[0])
+
+    all_trees=[]
+    slopes=[(1,1),(3,1),(5,1),(7,1),(1,2)]
+
+    for i in slopes:
+        x=i[0]
+        y=i[1]
+
+        trees=0
+        for _ in range(0, len(mountain)-1):
+            if(y>len(mountain)): break
+            if(mountain[y][x]=='#'): trees+=1
+            x=(x+i[0])%len_row
+            y+=i[1]
+        all_trees.append(trees)
+
+mul=1
+for i in all_trees: mul*=i
+
+print(mul)
